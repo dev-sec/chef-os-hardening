79853fddd1fe732cfbfb68edaaf2ef6a19e07b24
Bernhard K. Weisshuhn <bkw@codingforce.com>
2013-11-15 10:12:05 +0100

    add sysctl community cookbook

--- metadata.rb
+++ metadata.rb
@@ -4,4 +4,7 @@ maintainer_email "dominik.richter@googlemail.com"
 license          "Apache 2.0"
 description      "Installs/Configures security"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.0.1"
+version          "0.0.2"
+
+depends 'sysctl', '0.3.4'
+
