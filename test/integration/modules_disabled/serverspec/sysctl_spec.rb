240657d85aaf84ce6c7af46d3a8d0c17adde0682
Bernhard K. Weisshuhn <bkw@codingforce.com>
2013-11-20 16:29:48 +0100

    mode inited tests to own file, skip on redhat

--- test/integration/modules_disabled/serverspec/sysctl_spec.rb
+++ test/integration/modules_disabled/serverspec/sysctl_spec.rb
@@ -1,12 +1,6 @@
 require 'spec_helper'
 
-describe 'kernel modules' do
-
-    describe file('/etc/initramfs-tools/modules') do
-        its(:content) { should match /^ghash-clmulni-intel/ }
-        its(:content) { should match /^aesni-intel/ }
-        its(:content) { should match /^kvm-intel/ }
-    end
+describe 'System sysctl' do
 
     context linux_kernel_parameter('kernel.modules_disabled') do
         its(:value) { should eq 1 }
