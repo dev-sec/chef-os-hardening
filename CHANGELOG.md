54240d63bced66ffe7de610a31c299c83c6d2f68
Dominik Richter <dominik.richter@gmail.com>
2014-04-09 16:27:20 +0200

    version bump to 1.0.0
    Signed-off-by: Dominik Richter <dominik.richter@gmail.com>

---
+++ CHANGELOG.md
@@ -0,0 +1,5 @@
+# Changelog
+
+## 1.0.0
+
+* imported hardening project and updated to current version with full test suite
--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email "dominik.richter@googlemail.com"
 license          "Apache 2.0"
 description      "Installs/Configures security"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.0.2"
+version          "1.0.0"
 
 depends 'sysctl', '0.3.4'
 depends 'ntp'
