d1fcd6c30050a32b1e49eb02a02412cade9cbc7f
Edmund Haselwanter <me@ehaselwanter.com>
2014-06-22 21:16:32 +0200

    Prefer next in loops instead of conditional blocks.

--- libraries/gpgcheck.rb
+++ libraries/gpgcheck.rb
@@ -25,11 +25,12 @@ module Chef
       def self.check(file)
         pattern = /gpgcheck\s*=\s*0/
 
-    if File.file?(file)
-      File.open(file) do |f|
-        f.each_line do |line|
-          if pattern.match(line)
-            Chef::Log.warn file + ': gpgcheck=1 not properly configured'
+        if File.file?(file)
+          File.open(file) do |f|
+            f.each_line do |line|
+              next unless pattern.match(line)
+              Chef::Log.warn file + ': gpgcheck=1 not properly configured'
+            end
           end
         end
       end
