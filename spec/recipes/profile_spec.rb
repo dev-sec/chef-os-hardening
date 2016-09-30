c3f76adb8440abe88af6844349b889ed2d78ce2b
Grant Ridder <shortdudey123@gmail.com>
2016-09-30 15:53:20 -0700

    Expand chefspec testing
    ```
    ChefSpec Coverage report generated...
    
      Total Resources:   17
      Touched Resources: 17
      Touch Coverage:    100.0%
    ```

---
+++ spec/recipes/profile_spec.rb
@@ -0,0 +1,33 @@
+# encoding: UTF-8
+#
+# Copyright 2014, Deutsche Telekom AG
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+require_relative '../spec_helper'
+
+describe 'os-hardening::profile' do
+  let(:chef_run) do
+    ChefSpec::ServerRunner.new.converge(described_recipe)
+  end
+
+  it 'create /etc/profile.d/pinerolo_profile.sh' do
+    expect(chef_run).to create_template('/etc/profile.d/pinerolo_profile.sh').with(
+      source: 'profile.conf.erb',
+      mode: 0755,
+      owner: 'root',
+      group: 'root'
+    )
+  end
+end
---
+++ spec/recipes/suid_sgid_spec.rb
@@ -0,0 +1,28 @@
+# encoding: UTF-8
+#
+# Copyright 2014, Deutsche Telekom AG
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+require_relative '../spec_helper'
+
+describe 'os-hardening::suid_sgid' do
+  let(:chef_run) do
+    ChefSpec::ServerRunner.new.converge(described_recipe)
+  end
+
+  it 'run remove_suid_from_blacklists ruby_block' do
+    expect(chef_run).to run_ruby_block('remove_suid_from_blacklists')
+  end
+end
