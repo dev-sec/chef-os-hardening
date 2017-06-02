8572c05dda81a337f55c2a29e2d30fce434a1f06
Artem Sidorenko <artem@posteo.de>
2017-06-02 09:41:02 +0200

    Making foodcritic happy
    Signed-off-by: Artem Sidorenko <artem@posteo.de>

--- metadata.rb
+++ metadata.rb
@@ -19,10 +19,12 @@
 name             'os-hardening'
 maintainer       'Dominik Richter'
 maintainer_email 'dominik.richter@googlemail.com'
-license          'Apache 2.0'
+license          'Apache-2.0'
 description      'Installs and configures operating system hardening'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '2.0.1'
+
+chef_version '>= 12.5' if respond_to?(:chef_version)
 
 supports 'ubuntu', '>= 12.04'
 supports 'debian', '>= 6.0'
